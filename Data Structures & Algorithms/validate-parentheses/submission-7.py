affe869b3324895e0e53696cac2fab27c3973624
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-09-02 16:12:56 +0200

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        seen = [] # lista na nawias otwierający
+        pairs = {"]": "[", "}": "{", ")": "("}
+
+        for ch in s:
+            if ch not in pairs: # czy jest nawiasem otwierającym?
+                seen.apend(ch) # dodaj go do listy
+
+            elif not seen or seen.pop() != pairs[ch]: # dla nawiasu zamykającego sprawdź,
+                                                     # czy lista jest pusta lub ostatni nawias 
+                                                     # otwierający nie jest tego samego rodzaju
+                return False
+        return not seen
+
