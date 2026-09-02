c8e8156ad5120e21f9f2fbe67cf4ff1cdba41e7a
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-09-02 14:20:39 +0200

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        seen = []
+        open = {"[": 1, "{": 2, "(": 3}
+        close = {"]": 1, "}": 2, ")": 3}
+        for index in s:
+            if index in open:
+                seen.append(index)
+            else:
+                if close[index] != open[seen[-1]]:
+                    return False
+        return True
+
+
