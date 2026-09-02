fda7ec607e49d6d975cc49ed946d8b6fbe2c8fa7
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-09-02 13:22:07 +0200

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+            min_cena = prices[0]
+            max_zysk = 0
+
+            for price in prices[1:]:
+                if price < min_cena:
+                    min_cena = price
+                else:
+                    max_zysk = max(max_zysk, cena - min_cena)
+            
+            return max_zysk
