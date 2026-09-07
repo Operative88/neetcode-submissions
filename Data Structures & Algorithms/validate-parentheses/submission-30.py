acd237d0b587b9aa45a679e400a1284417e0ca89
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-09-07 11:38:53 +0200

    Add: validate-parentheses - submission-30

---
+++ Data Structures & Algorithms/validate-parentheses/submission-30.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        pairs = {'}': '{', ')': '(', ']': '['}
+
+        seen = []
+
+        for nawias in s:
+            if nawias not in pairs:
+                seen.append(nawias)
+            else:
+                if not seen or pairs[nawias] != seen.pop():
+                    return False 
+        
+        if len(seen) == len(s) or seen:
+            return False
+        else:
+            return True
+# "["
