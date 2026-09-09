68e222eefcd7f365dd0d66d8a19c34daa7990007
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-09-09 11:24:44 +0200

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        t = [0] * 26
+
+        r = s[::-1]
+
+        for a, b in zip(s, r):
+            if a.isalnum():
+                t[ord(a) - ord('a')] += 1
+                t[ord(r) - ord('a')] -= 1
+
+
+        return sum(s) == 0 
