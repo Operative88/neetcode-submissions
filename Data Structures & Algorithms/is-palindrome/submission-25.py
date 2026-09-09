9d48c3cb17c7188283286458031f4ddb005ade20
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-09-09 17:49:51 +0200

    Add: is-palindrome - submission-25

---
+++ Data Structures & Algorithms/is-palindrome/submission-25.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        t = [0] * 500
+
+        r = s[::-1] 
+
+        
+
+        for a in s:
+
+            if a.isalnum():
+            
+                t[ord(a.lower()) - ord('a')] += 1
+                t[ord(b.lower()) - ord('a')] -= 1
+
+
+        return sum(t) == 0 
+        
+
+
+
+    
